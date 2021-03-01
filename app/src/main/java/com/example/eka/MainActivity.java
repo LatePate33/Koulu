f0b9e8bb2a2fcd267105efb33a2ddc2f55e769ab
LatePate33 <tirkkonenlauri@gmail.com>
2021-03-01 19:59:52 +0200

    Tehtava 2

--- app/src/main/java/com/example/eka/MainActivity.java
+++ app/src/main/java/com/example/eka/MainActivity.java
@@ -4,19 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this.print();
+        text = (TextView) findViewById(R.id.textView2);
+
     }
 
 
     public void print(View v) {
-        System.out.println("Hello world!");
+        text.setText("Hello world!");
     }
 
 }
