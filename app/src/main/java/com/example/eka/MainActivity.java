0693b4b814b3717f01e308b5b774ecf746354972
LatePate33 <tirkkonenlauri@gmail.com>
2021-03-01 19:39:47 +0200

    Tehtava 1

---
+++ app/src/main/java/com/example/eka/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.eka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //this.print();
+    }
+
+
+    public void print(View v) {
+        System.out.println("Hello world!");
+    }
+
+}
