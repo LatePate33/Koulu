13b501db39389404192519a91d3fca3247029397
LatePate33 <tirkkonenlauri@gmail.com>
2021-03-02 14:09:40 +0200

    Tehtävä 4

--- app/src/main/java/com/example/eka/MainActivity.java
+++ app/src/main/java/com/example/eka/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.eka;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.security.Key;
 import java.util.Scanner;
+
+import static android.view.KeyEvent.KEYCODE_ENTER;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +25,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = (TextView) findViewById(R.id.textView2);
-        text2 = (EditText) findViewById(R.id.syotto);
 
     }
 
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        text = (TextView) findViewById(R.id.textView2);
+        text2 = (EditText) findViewById(R.id.syotto);
+        if (keyCode == KEYCODE_ENTER) {
+            print();
+        }
+        return true;
+    }
 
-    public void print(View v) {
+
+    public void print() {
         String sana = "";
         sana += text2.getText();
         text.setText(sana);
     }
-
 }
